5af036bf56acae2c05f7c735da410f7415b2df6a
Jacob Ranson <contact@jacobranson.com>
2021-10-30 00:14:16 -0400

    Bevy "Hello, world!"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use bevy::prelude::{App, IntoSystem};
+
 fn main() {
+    App::build()
+        .add_system(hello_world.system())
+        .run();
+}
+
+fn hello_world() {
     println!("Hello, world!");
 }
