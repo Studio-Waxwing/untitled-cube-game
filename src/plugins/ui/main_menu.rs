5495eb698c2d17f45a91a2e5cb3b09f17c1f8e6e
Jacob Ranson <contact@jacobranson.com>
2021-11-03 19:58:41 -0400

    Reorganize systems

--- src/plugins/ui/main_menu.rs
+++ src/plugins/ui/main_menu.rs
@@ -5,22 +5,25 @@ pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app
-          .add_startup_system(setup.system());
+          .add_startup_system(spawn_camera.system())
+          .add_startup_system(spawn_menu.system());
     }
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+fn spawn_camera(mut commands: Commands) {
+  commands.spawn_bundle(UiCameraBundle::default());
+}
+
+fn spawn_menu(
+  mut commands: Commands,
+  mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn_bundle(UiCameraBundle::default());
-    commands.spawn_bundle(NodeBundle {
-      style: Style {
-        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-        ..Default::default()
-      },
-      material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
+  commands.spawn_bundle(NodeBundle {
+    style: Style {
+      size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
       ..Default::default()
-    });
+    },
+    material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
+    ..Default::default()
+  });
 }
