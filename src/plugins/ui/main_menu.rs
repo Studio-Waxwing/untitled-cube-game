8e21f40c3cc712628a1745acf803550f8e3f1658
Jacob Ranson <contact@jacobranson.com>
2021-11-04 19:02:01 -0400

    Added materials, options button

--- src/plugins/ui/main_menu.rs
+++ src/plugins/ui/main_menu.rs
@@ -4,7 +4,7 @@ pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.init_resource::<ButtonMaterials>()
+        app.init_resource::<MenuMaterials>()
             .add_startup_system(spawn_camera_system.system())
             .add_startup_system(spawn_menu_system.system())
             .add_system(button_system.system())
@@ -18,15 +18,17 @@ fn spawn_camera_system(mut commands: Commands) {
 
 fn spawn_menu_system(
     mut commands: Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
+    menu_materials: Res<MenuMaterials>,
 ) {
-    let root_container = create_root_container(&mut materials);
-    let buttons_container = create_buttons_container(&mut materials);
-    let new_game_button = create_button(&mut materials);
-    let new_game_button_text = create_button_text(&asset_server);
-    let exit_game_button = create_button(&mut materials);
-    let exit_game_button_text = create_button_text(&asset_server);
+    let root_container = create_root_container(&menu_materials);
+    let buttons_container = create_buttons_container();
+    let new_game_button = create_first_button(&menu_materials);
+    let new_game_button_text = create_button_text(&asset_server, "New Game");
+    let options_button = create_button(&menu_materials);
+    let options_button_text = create_button_text(&asset_server, "Options");
+    let exit_game_button = create_button(&menu_materials);
+    let exit_game_button_text = create_button_text(&asset_server, "Exit Game");
 
     commands
         .spawn_bundle(root_container)
@@ -40,6 +42,11 @@ fn spawn_menu_system(
                             parent.spawn_bundle(new_game_button_text);
                         });
                     parent
+                        .spawn_bundle(options_button)
+                        .with_children(|parent| {
+                            parent.spawn_bundle(options_button_text);
+                        });
+                    parent
                         .spawn_bundle(exit_game_button)
                         .with_children(|parent| {
                             parent.spawn_bundle(exit_game_button_text);
@@ -48,7 +55,7 @@ fn spawn_menu_system(
         });
 }
 
-fn create_root_container(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeBundle {
+fn create_root_container(menu_materials: &Res<MenuMaterials>) -> NodeBundle {
     NodeBundle {
         style: Style {
             display: Display::Flex,
@@ -58,40 +65,60 @@ fn create_root_container(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeB
             justify_content: JustifyContent::Center,
             ..Default::default()
         },
-        material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
+        material: menu_materials.background.clone(),
         ..Default::default()
     }
 }
 
-fn create_buttons_container(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeBundle {
+fn create_buttons_container() -> NodeBundle {
     NodeBundle {
         style: Style {
-            flex_direction: FlexDirection::Column,
+            min_size: Size::new(Val::Px(250.0), Val::Auto),
+            flex_direction: FlexDirection::ColumnReverse,
             ..Default::default()
         },
-        material: materials.add(Color::rgb(0.65, 0.65, 0.0).into()),
         ..Default::default()
     }
 }
 
-fn create_button(materials: &mut ResMut<Assets<ColorMaterial>>) -> ButtonBundle {
+fn create_first_button(menu_materials: &Res<MenuMaterials>) -> ButtonBundle {
     ButtonBundle {
         style: Style {
-            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+            flex_grow: 1.0,
+            padding: Rect::all(Val::Px(5.0)),
             margin: Rect::all(Val::Px(5.0)),
             justify_content: JustifyContent::Center,
             align_items: AlignItems::Center,
             ..Default::default()
         },
-        material: materials.add(Color::rgb(0.65, 0.0, 0.65).into()),
+        material: menu_materials.button_normal.clone(),
         ..Default::default()
     }
 }
 
-fn create_button_text(asset_server: &Res<AssetServer>) -> TextBundle {
+fn create_button(menu_materials: &Res<MenuMaterials>) -> ButtonBundle {
+    ButtonBundle {
+        style: Style {
+            padding: Rect::all(Val::Px(5.0)),
+            margin: Rect {
+                left: Val::Px(5.0),
+                right: Val::Px(5.0),
+                top: Val::Px(0.0),
+                bottom: Val::Px(5.0),
+            },
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..Default::default()
+        },
+        material: menu_materials.button_normal.clone(),
+        ..Default::default()
+    }
+}
+
+fn create_button_text(asset_server: &Res<AssetServer>, text: &str) -> TextBundle {
     TextBundle {
         text: Text::with_section(
-            "Button",
+            text,
             TextStyle {
                 font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                 font_size: 40.0,
@@ -103,26 +130,28 @@ fn create_button_text(asset_server: &Res<AssetServer>) -> TextBundle {
     }
 }
 
-struct ButtonMaterials {
-    normal: Handle<ColorMaterial>,
-    hovered: Handle<ColorMaterial>,
-    pressed: Handle<ColorMaterial>,
+struct MenuMaterials {
+    background: Handle<ColorMaterial>,
+    button_normal: Handle<ColorMaterial>,
+    button_hovered: Handle<ColorMaterial>,
+    button_pressed: Handle<ColorMaterial>,
 }
 
-impl FromWorld for ButtonMaterials {
+impl FromWorld for MenuMaterials {
     fn from_world(world: &mut World) -> Self {
         let mut materials = world.get_resource_mut::<Assets<ColorMaterial>>().unwrap();
 
-        ButtonMaterials {
-            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
-            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
-            pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
+        MenuMaterials {
+            background: materials.add(Color::hex("082032").unwrap().into()),
+            button_normal: materials.add(Color::hex("2C394B").unwrap().into()),
+            button_hovered: materials.add(Color::hex("334756").unwrap().into()),
+            button_pressed: materials.add(Color::hex("FF4C29").unwrap().into()),
         }
     }
 }
 
 fn button_system(
-    button_materials: Res<ButtonMaterials>,
+    menu_materials: Res<MenuMaterials>,
     mut interaction_query: Query<
         (&Interaction, &mut Handle<ColorMaterial>),
         (Changed<Interaction>, With<Button>),
@@ -130,9 +159,9 @@ fn button_system(
 ) {
     for (interaction, mut material) in interaction_query.iter_mut() {
         *material = match *interaction {
-            Interaction::Clicked => button_materials.pressed.clone(),
-            Interaction::Hovered => button_materials.hovered.clone(),
-            Interaction::None => button_materials.normal.clone(),
+            Interaction::Clicked => menu_materials.button_pressed.clone(),
+            Interaction::Hovered => menu_materials.button_hovered.clone(),
+            Interaction::None => menu_materials.button_normal.clone(),
         }
     }
 }
