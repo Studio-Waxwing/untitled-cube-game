d0585320161448dd1b5a23505c8c129596594219
Jacob Ranson <contact@jacobranson.com>
2021-11-03 21:13:49 -0400

    Cleaned up element code

--- src/plugins/ui/main_menu.rs
+++ src/plugins/ui/main_menu.rs
@@ -14,59 +14,53 @@ fn spawn_camera(mut commands: Commands) {
 }
 
 fn spawn_menu(
-    commands: Commands,
-    materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
-) {
-    root_container(commands, materials, asset_server);
-}
-
-fn root_container(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
+    let root_container = root_container(&mut materials);
+    let buttons_container = buttons_container(&mut materials);
+    let new_game_button = new_game_button(&mut materials);
+
     commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                display: Display::Flex,
-                flex_grow: 1.0,
-                flex_shrink: 0.0,
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                ..Default::default()
-            },
-            material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
-            ..Default::default()
-        })
+        .spawn_bundle(root_container)
         .with_children(|parent| {
-            buttons_container(materials, asset_server, parent);
+            parent
+                .spawn_bundle(buttons_container)
+                .with_children(|parent| {
+                    parent.spawn_bundle(new_game_button);
+                });
         });
 }
 
-fn buttons_container(
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
-    parent: &mut ChildBuilder,
-) {
-    parent.spawn_bundle(NodeBundle {
+fn root_container(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeBundle {
+    NodeBundle {
+        style: Style {
+            display: Display::Flex,
+            flex_grow: 1.0,
+            flex_shrink: 0.0,
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..Default::default()
+        },
+        material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
+        ..Default::default()
+    }
+}
+
+fn buttons_container(materials: &mut ResMut<Assets<ColorMaterial>>) -> NodeBundle {
+    NodeBundle {
         style: Style {
             size: Size::new(Val::Px(450.0), Val::Px(450.0)),
             ..Default::default()
         },
         material: materials.add(Color::rgb(0.65, 0.65, 0.0).into()),
         ..Default::default()
-    }).with_children(|parent| {
-        new_game_button(materials, asset_server, parent);
-    });
+    }
 }
 
-fn new_game_button(
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
-    parent: &mut ChildBuilder,
-) {
-    parent.spawn_bundle(ButtonBundle {
+fn new_game_button(materials: &mut ResMut<Assets<ColorMaterial>>) -> ButtonBundle {
+    ButtonBundle {
         style: Style {
             size: Size::new(Val::Px(150.0), Val::Px(65.0)),
             // center button
@@ -79,5 +73,5 @@ fn new_game_button(
         },
         material: materials.add(Color::rgb(0.65, 0.0, 0.65).into()),
         ..Default::default()
-    });
+    }
 }
