f9a818d53f505f5e7a2e86fac32b4524c78f94c0
Jacob Ranson <contact@jacobranson.com>
2021-11-03 20:32:55 -0400

    Blocking out some ui

--- src/plugins/ui/main_menu.rs
+++ src/plugins/ui/main_menu.rs
@@ -14,6 +14,14 @@ fn spawn_camera(mut commands: Commands) {
 }
 
 fn spawn_menu(
+    commands: Commands,
+    materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    root_container(commands, materials, asset_server);
+}
+
+fn root_container(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
@@ -21,50 +29,55 @@ fn spawn_menu(
     commands
         .spawn_bundle(NodeBundle {
             style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                display: Display::Flex,
+                flex_grow: 1.0,
+                flex_shrink: 0.0,
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
                 ..Default::default()
             },
             material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
             ..Default::default()
         })
         .with_children(|parent| {
-            child1(materials, asset_server, parent);
+            buttons_container(materials, asset_server, parent);
         });
 }
 
-fn child1(
+fn buttons_container(
     mut materials: ResMut<Assets<ColorMaterial>>,
     asset_server: Res<AssetServer>,
     parent: &mut ChildBuilder,
 ) {
-    // left vertical fill (content)
-    parent
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                align_items: AlignItems::FlexEnd,
-                ..Default::default()
-            },
-            material: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+    parent.spawn_bundle(NodeBundle {
+        style: Style {
+            size: Size::new(Val::Px(450.0), Val::Px(450.0)),
             ..Default::default()
-        })
-        .with_children(|parent| {
-            // text
-            parent.spawn_bundle(TextBundle {
-                style: Style {
-                    margin: Rect::all(Val::Px(5.0)),
-                    ..Default::default()
-                },
-                text: Text::with_section(
-                    "Text Example",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 30.0,
-                        color: Color::WHITE,
-                    },
-                    Default::default(),
-                ),
-                ..Default::default()
-            });
-        });
+        },
+        material: materials.add(Color::rgb(0.65, 0.65, 0.0).into()),
+        ..Default::default()
+    }).with_children(|parent| {
+        new_game_button(materials, asset_server, parent);
+    });
+}
+
+fn new_game_button(
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    parent: &mut ChildBuilder,
+) {
+    parent.spawn_bundle(ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+            // center button
+            margin: Rect::all(Val::Auto),
+            // horizontally center child text
+            justify_content: JustifyContent::Center,
+            // vertically center child text
+            align_items: AlignItems::Center,
+            ..Default::default()
+        },
+        material: materials.add(Color::rgb(0.65, 0.0, 0.65).into()),
+        ..Default::default()
+    });
 }
