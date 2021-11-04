29d77f56003d3622b61b00c877ba2ff07e35be28
Jacob Ranson <contact@jacobranson.com>
2021-11-03 20:06:13 -0400

    Break up UI into separate functions

--- src/plugins/ui/main_menu.rs
+++ src/plugins/ui/main_menu.rs
@@ -4,26 +4,67 @@ pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app
-          .add_startup_system(spawn_camera.system())
-          .add_startup_system(spawn_menu.system());
+        app.add_startup_system(spawn_camera.system())
+            .add_startup_system(spawn_menu.system());
     }
 }
 
 fn spawn_camera(mut commands: Commands) {
-  commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(UiCameraBundle::default());
 }
 
 fn spawn_menu(
-  mut commands: Commands,
-  mut materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
-  commands.spawn_bundle(NodeBundle {
-    style: Style {
-      size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-      ..Default::default()
-    },
-    material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
-    ..Default::default()
-  });
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                ..Default::default()
+            },
+            material: materials.add(Color::rgb(0.65, 0.0, 0.0).into()),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            child1(materials, asset_server, parent);
+        });
+}
+
+fn child1(
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+    parent: &mut ChildBuilder,
+) {
+    // left vertical fill (content)
+    parent
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::FlexEnd,
+                ..Default::default()
+            },
+            material: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            // text
+            parent.spawn_bundle(TextBundle {
+                style: Style {
+                    margin: Rect::all(Val::Px(5.0)),
+                    ..Default::default()
+                },
+                text: Text::with_section(
+                    "Text Example",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 30.0,
+                        color: Color::WHITE,
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        });
 }
